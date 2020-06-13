9d91137256d0ab77501dbd6e203b39071dd2a13b
catalunha <catalunha.mj@gmail.com>
2020-06-13 17:38:24 -0300

    pagina de login ainda nao buscando email e password

---
+++ lib/presentation/login_page_ui.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pmsb4/presentation/login_button_ui.dart';
+
+class LoginPageUI extends StatelessWidget {
+  final Function login;
+
+  const LoginPageUI({Key key, this.login}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            LoginButtonUI(
+              onPressedCallBack: login,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
