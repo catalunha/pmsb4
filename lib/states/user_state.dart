41ebd7745a702d53e5faab6891a01814afafc528
catalunha <catalunha.mj@gmail.com>
2020-06-13 17:36:31 -0300

    acrescentei userModel do firebase mas ainda nao alimentei com middleware

--- lib/states/user_state.dart
+++ lib/states/user_state.dart
@@ -1,28 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
+import 'package:pmsb4/database/user/user_model.dart';
 
 @immutable
 class UserState {
   final FirebaseUser firebaseUser;
-
+  final UserModel userModel;
   UserState({
     this.firebaseUser,
+    this.userModel,
   });
   factory UserState.initial() {
-    return UserState(firebaseUser: null);
+    return UserState(firebaseUser: null, userModel: null);
   }
-  UserState copyWith({FirebaseUser firebaseUser}) {
-    return UserState(
-      firebaseUser: firebaseUser,
-    );
+  UserState copyWith({FirebaseUser firebaseUser, UserModel userModel}) {
+    return UserState(firebaseUser: firebaseUser, userModel: userModel);
   }
 
   @override
-  int get hashCode => firebaseUser.hashCode;
+  int get hashCode => firebaseUser.hashCode ^ userModel.hashCode;
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
       other is UserState &&
           runtimeType == other.runtimeType &&
-          firebaseUser == other.firebaseUser;
+          firebaseUser == other.firebaseUser &&
+          userModel == other.userModel;
 }
