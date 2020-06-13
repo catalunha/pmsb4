9e17644a6df501a899b32b10cbffb4b130e369b6
catalunha <catalunha.mj@gmail.com>
2020-06-13 17:45:31 -0300

    importante usar navegação por keys marcando isto no main. assim podemos navegar tanto em context quanto em actions

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
+
 class Routes{
   static final home = '/';
-  static final login = '/login';
+  static final perfil = '/perfil';
   static final counter = '/counter';
+}
+class Keys {
+  static final navKey = GlobalKey<NavigatorState>();
 }
