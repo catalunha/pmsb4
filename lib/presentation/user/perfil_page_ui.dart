14d8f655414a41b632b9200bba03f6bedfdf2654
catalunha <catalunha.mj@gmail.com>
2020-06-13 17:37:00 -0300

    criando page de perfil para testes

---
+++ lib/presentation/user/perfil_page_ui.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PerfilPageUI extends StatelessWidget {
+  final String displayName;
+  final String email;
+  final String phoneNumber;
+  final String photoUrl;
+  PerfilPageUI(
+      {Key key, this.displayName, this.email, this.phoneNumber, this.photoUrl})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil do Usuario'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('FirebaseUser'),
+            subtitle: Text('displayName:$displayName\nemail:$email\nphoneNumber:$phoneNumber\nphotoUrl:$photoUrl'),
+          )
+        ],
+      ),
+    );
+  }
+}
