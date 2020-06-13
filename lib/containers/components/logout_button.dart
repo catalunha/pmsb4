830b7b24605025f1e8bf929f18e99d01bb5643c7
catalunha <catalunha.mj@gmail.com>
2020-06-13 17:38:55 -0300

    botao de logout em qq lugar

--- lib/containers/components/logout_button.dart
+++ lib/containers/components/logout_button.dart
@@ -2,9 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:pmsb4/actions/user_action.dart';
 import 'package:pmsb4/presentation/components/logout_button_ui.dart';
+import 'package:pmsb4/routes.dart';
 import 'package:pmsb4/states/app_state.dart';
 import 'package:redux/redux.dart';
+class _ViewModel {
+  final Function logout;
 
+  _ViewModel({this.logout});
+
+  static _ViewModel fromStore(Store<AppState> store) {
+    return _ViewModel(logout: () {
+      store.dispatch(UserLogout());
+      Keys.navKey.currentState.pushNamed(Routes.home);
+    });
+  }
+}
 class LogoutButton extends StatelessWidget {
   LogoutButton({Key key}) : super(key: key);
   @override
@@ -20,14 +32,4 @@ class LogoutButton extends StatelessWidget {
   }
 }
 
-class _ViewModel {
-  final Function logout;
 
-  _ViewModel({this.logout});
-
-  static _ViewModel fromStore(Store<AppState> store) {
-    return _ViewModel(logout: () {
-      store.dispatch(UserLogout());
-    });
-  }
-}
