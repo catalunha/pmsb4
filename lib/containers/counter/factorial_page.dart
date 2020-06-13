15cb4ee69739c43268f72e0aca3ebdf0554ae410
catalunha <catalunha.mj@gmail.com>
2020-06-13 17:39:57 -0300

    ainda definindo uma terceira pagina para navegação do counter

---
+++ lib/containers/counter/factorial_page.dart
@@ -0,0 +1,14 @@
+import 'package:pmsb4/selectors/counter/selector.dart';
+import 'package:pmsb4/states/app_state.dart';
+import 'package:redux/redux.dart';
+
+class _ViewModel {
+  final int factorial;
+
+  _ViewModel({this.factorial});
+  static _ViewModel fromStore(Store<AppState> store) {
+    return _ViewModel(
+        factorial: factorialCounterSelector(store.state.counterState));
+  }
+}
+
