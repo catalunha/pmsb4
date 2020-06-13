6f9acd690f565154a248d3c7be51f4671b275e21
catalunha <catalunha.mj@gmail.com>
2020-06-13 17:46:42 -0300

    criando userModel q vai ser conectado no firestore

---
+++ lib/database/user/user_model.dart
@@ -0,0 +1,35 @@
+import 'package:pmsb4/database/firestore_model.dart';
+
+class UserModel extends FirestoreModel {
+  static final String collection = 'Usuario';
+  String nickname;
+
+  UserModel(
+    String id, {
+    this.nickname,
+  }) : super(id);
+  @override
+  UserModel fromMap(Map<String, dynamic> map) {
+    if (map.containsKey('nickname')) nickname = map['nickname'];
+    return this;
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (nickname != null) data['nickname'] = this.nickname;
+    return data;
+  }
+  @override
+  UserModel fromFirestore(Map<String, dynamic> map) {
+    if (map.containsKey('nome')) nickname = map['nome'];
+    return this;
+  }
+
+  @override
+  Map<String, dynamic> toFirestore() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (nickname != null) data['nome'] = this.nickname;
+    return data;
+  }
+}
