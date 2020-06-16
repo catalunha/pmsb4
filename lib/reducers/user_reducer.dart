f23dbb2cfa639eae95d9240ea9d8e1b2a1b72272
catalunha <catalunha.mj@gmail.com>
2020-06-16 14:48:56 -0300

    organizando nomes em userreducer

--- lib/reducers/user_reducer.dart
+++ lib/reducers/user_reducer.dart
@@ -4,24 +4,29 @@ import 'package:pmsb4/states/user_state.dart';
 import 'package:redux/redux.dart';
 
 final userReducer = combineReducers<UserState>([
-  TypedReducer<UserState, UserAuthenticationStatusAction>(_authentication),
-  TypedReducer<UserState, UserUpdateProfileSuccessfulAction>(_updateProfile),
-  TypedReducer<UserState, UserLoginSuccessfulAction>(_login),
-  TypedReducer<UserState, UserLoginFailAction>(_loginFail),
-  TypedReducer<UserState, UserLogoutSuccessfulAction>(_logout),
+  TypedReducer<UserState, UserAuthenticationStatusAction>(
+      _userAuthenticationStatusAction),
+  TypedReducer<UserState, UserUpdateProfileSuccessfulAction>(
+      _userUpdateProfileSuccessfulAction),
+  TypedReducer<UserState, UserLoginSuccessfulAction>(
+      _userLoginSuccessfulAction),
+  TypedReducer<UserState, UserLoginFailAction>(_userLoginFailAction),
+  TypedReducer<UserState, UserLogoutSuccessfulAction>(
+      _userLogoutSuccessfulAction),
 ]);
 
-UserState _updateProfile(
+UserState _userUpdateProfileSuccessfulAction(
     UserState state, UserUpdateProfileSuccessfulAction action) {
   return state.copyWith(firebaseUser: action.firebaseUser);
 }
 
-UserState _authentication(
+UserState _userAuthenticationStatusAction(
     UserState state, UserAuthenticationStatusAction action) {
   return state.copyWith(authenticationStatus: action.authenticationStatus);
 }
 
-UserState _login(UserState state, UserLoginSuccessfulAction action) {
+UserState _userLoginSuccessfulAction(
+    UserState state, UserLoginSuccessfulAction action) {
   print(state.authenticationStatus.toString());
   print(state.toString());
 
@@ -30,7 +35,8 @@ UserState _login(UserState state, UserLoginSuccessfulAction action) {
       firebaseUser: action.firebaseUser);
 }
 
-UserState _logout(UserState state, UserLogoutSuccessfulAction action) {
+UserState _userLogoutSuccessfulAction(
+    UserState state, UserLogoutSuccessfulAction action) {
   // print(state.authenticationStatus.toString());
   print(state.toString());
   return state.copyWith(
@@ -38,7 +44,7 @@ UserState _logout(UserState state, UserLogoutSuccessfulAction action) {
       firebaseUser: null);
 }
 
-UserState _loginFail(UserState state, UserLoginFailAction action) {
+UserState _userLoginFailAction(UserState state, UserLoginFailAction action) {
   print('_loginFail');
   print(state.authenticationStatus.toString());
 
